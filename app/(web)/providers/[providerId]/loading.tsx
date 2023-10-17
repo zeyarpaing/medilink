386e59aff55ba803a84490f3d889289cd0895abe
ZeyarPaing <zeyarpaing@protonmail.com>
2023-10-17 20:38:51 +0630

    feat: loading for hospital detail page

--- app/(web)/providers/[providerId]/loading.tsx
+++ app/(web)/providers/[providerId]/loading.tsx
@@ -1,19 +1,26 @@
+import { Skeleton } from '@nextui-org/skeleton';
+
 export default async function Page() {
   return (
     <div className="mcontainer min-h-screen py-12">
       <div>
-        <div className="h-[32rem] w-full">
+        <Skeleton className="h-[32rem] w-full rounded-xl">
           <div className="h-full w-full rounded-xl bg-gray-500 object-cover" />
-        </div>
-        <div className="[&>*]bg-gray-500 my-8 [&>*]:h-4 [&>*]:min-w-[5rem] [&>*]:bg-gray-500">
-          <h1 className="mb-2 text-3xl font-black"> </h1>
-          <p className="text-gray-600"> </p>
-          <p className="mt-2 text-gray-700"> </p>
-          <p className="mt-2 text-gray-700"> </p>
-          <p className="mt-2 text-gray-700"> </p>
+        </Skeleton>
+        <div className="my-8">
+          <Skeleton className="mb-2 h-12 w-48 rounded-lg"> </Skeleton>
+          <Skeleton className="mb-2 mt-3 h-5 w-32 rounded-lg "> </Skeleton>
+          <Skeleton className="mb-2 mt-3 h-5 w-32 rounded-lg "> </Skeleton>
+          <Skeleton className="mb-2 mt-3 h-5 w-32 rounded-lg "> </Skeleton>
+          <Skeleton className="mb-2 mt-3 h-5 w-32 rounded-lg "> </Skeleton>
         </div>
       </div>
       <h2 className="mb-4 mt-6 text-2xl font-bold">Services Offered</h2>
+      <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <Skeleton className="mb-4 h-24 w-full rounded-lg  text-sm"> </Skeleton>
+        <Skeleton className="mb-4 h-24 w-full rounded-lg  text-sm"> </Skeleton>
+        <Skeleton className="mb-4 h-24 w-full rounded-lg  text-sm"> </Skeleton>
+      </ul>
     </div>
   );
 }
